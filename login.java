10f50aab4cd80bd3f0e18c42c3a61da34fc72375
raj ubuntu <rajubuntu@gmail.com>
2021-01-23 05:04:21 +0530

    even branch can push on github and merge

--- login.java
+++ login.java
@@ -4,4 +4,5 @@ class A
 		version 2 testing by windows on github
 		clone by ubuntu local repository through github remote repo
 		windows developer pulled the updated v2 version file
+		This by other branch coding and then will merge both branch though UI
 }
