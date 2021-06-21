fadfb0c1b29336272acd32888b50d2fa045ac077
raj kumar <73242630+erajdevops@users.noreply.github.com>
2021-06-21 12:52:46 +0530

    Update login.java

--- login.java
+++ login.java
@@ -1,9 +1,5 @@
-class A
-{ ok
-	testing by github
-		version 2 testing by windows on github
-		clone by ubuntu local repository through github remote repo
-		windows developer pulled the updated v2 version file
-		This by other branch coding and then will merge both branch though UI
-		ssh window login on github update
+class Simple{
+public static void main(Strings args[]){	
+System.out.println("Welcome to Emobi Raj Kumar Devops Engineer");
+}	
 }
